23c21e5ca9bf3abeb5490a69b7f0fe8c32d457fd
maayanchechik <maayan.chechik@gmail.com>
2021-10-25 17:05:49 +0300

    Unet_3D debugged with test_3D_Unet

--- Unet_3D.py
+++ Unet_3D.py
@@ -26,27 +26,26 @@ class UNet_3D(nn.Module):
         self.conv8 = nn.Conv3d(256, 256, kernel_size = (3,3,3), padding = padding)
         self.gnorm8 = nn.GroupNorm(num_groups,256)
 
-        self.conv9 = nn.Conv3d(256, 128, kernel_size = (3,3,3), padding = padding)
+        self.conv9 = nn.Conv3d(384, 128, kernel_size = (3,3,3), padding = padding)
         self.gnorm9 = nn.GroupNorm(num_groups,128)
         self.conv10 = nn.Conv3d(128, 128, kernel_size = (3,3,3), padding = padding)
         self.gnorm10 = nn.GroupNorm(num_groups,128)
 
-        self.conv11 = nn.Conv3d(128, 64, kernel_size = (3,3,3), padding = padding)
+        self.conv11 = nn.Conv3d(192, 64, kernel_size = (3,3,3), padding = padding)
         self.gnorm11 = nn.GroupNorm(num_groups,64)
         self.conv12 = nn.Conv3d(64, 64, kernel_size = (3,3,3), padding = padding)
         self.gnorm12 = nn.GroupNorm(num_groups,64)
 
-        self.conv13 = nn.Conv3d(64, 32, kernel_size = (3,3,3), padding = padding)
+        self.conv13 = nn.Conv3d(96, 32, kernel_size = (3,3,3), padding = padding)
         self.gnorm13 = nn.GroupNorm(num_groups,32)
         self.conv14 = nn.Conv3d(32, 32, kernel_size = (3,3,3), padding = padding)
         self.gnorm14 = nn.GroupNorm(num_groups,32)
 
-        self.conv_last = nn.Conv3d(32, 4, kernel_size = (1,1,1)) #does this have padding?
+        self.conv_last = nn.Conv3d(32, 4, kernel_size = (1,1,1))
 
         #other
         self.relu = nn.ReLU()
-#        self.gnorm = nn.GroupNorm(num_groups,num_channels)
-        self. dropout = nn.Dropout3d()
+        self.dropout = nn.Dropout3d()
         self.final_activation = nn.Softmax(dim = 1)
         self.maxpool = nn.MaxPool3d(pool_kernal)
 
@@ -70,9 +69,10 @@ class UNet_3D(nn.Module):
         y8 = self.gnorm8(self.relu(self.conv8(y7)))
         y8_up = interpolate(y8, scale_factor = 2, mode='nearest')
         y8_con = torch.cat((y6, y8_up), dim=1)
-
+        
         y8_drop = self.dropout(y8_con)
-        y9 = self.gnorm9(self.relu(self.conv9(y8_drop)))
+        y91 = self.conv9(y8_drop)
+        y9 = self.gnorm9(self.relu(y91))
         y10 = self.gnorm10(self.relu(self.conv10(y9)))
         y10_up = interpolate(y10, scale_factor = 2, mode='nearest')
         y10_con = torch.cat((y4, y10_up), dim=1)
@@ -87,7 +87,7 @@ class UNet_3D(nn.Module):
         y13 = self.gnorm13(self.relu(self.conv13(y12_drop)))
         y14 = self.gnorm14(self.relu(self.conv14(y13)))
         cov_out = self.conv_last(y14)
-        activation_out = final_activation(y14)
+        activation_out = self.final_activation(y14)
 
         return cov_out, activation_out 
 
