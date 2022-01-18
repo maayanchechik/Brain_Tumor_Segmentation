9d6f6ec64f78dacd3b1003da6a5dfab4e2911664
maayanchechik <maayan.chechik@gmail.com>
2022-01-18 19:06:47 +0200

    some debugging

--- Res_Unet_3D.py
+++ Res_Unet_3D.py
@@ -3,15 +3,15 @@ import torch
 import torch.nn as nn
 def conv_block(in_channels, out_channels, kernel_size=(3,3,3), padding=1, num_groups=8):
   return nn.Sequential(
-     nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
-     nn.GroupNorm(num_groups, out_channels),
-     nn.ReLU()#inplace? will this enable more memory?
-     )
+    nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+    nn.GroupNorm(num_groups, out_channels),
+    nn.ReLU()#inplace? will this enable more memory?
+  )
 def double_conv(in_channels, out_channels, kernel_size=(3,3,3), padding=1, num_groups=8):
   return nn.Sequential(
     conv_block(in_channels, out_channels, kernel_size, padding, num_groups),
     conv_block(out_channels, out_channels, kernel_size, padding, num_groups)
-    )
+  )
 
 class res_block(nn.Module):
   def __init__(self, in_channels, out_channels, kernel_size=(3,3,3), padding=1, num_groups=8):
@@ -50,39 +50,38 @@ class decoder_block(nn.Module):
     return x
 
 class Res_Unet(nn.Module):
-    def __init__(self, in_channels=4, out_channels=[32,64,128,256], kernel_size=(3,3,3), padding=1, num_groups=8, pool_kernal=2):
-        super(Res_Unet, self).__init__()
-        encoder_list = []
-        for i in range(len(out_channels)):
-            if i==0:
-                encoder = res_block(in_channels, out_channels[i], kernel_size, padding, num_groups)
-            else:
-                encoder = encoder_block(out_channels[i-1], out_channels[i], kernel_size, padding, num_groups, pool_kernal)
-            encoder_list.append(encoder)
-        self.encoders = nn.ModuleList(encoder_list)
+  def __init__(self, in_channels=4, out_channels=[32,64,128,256], kernel_size=(3,3,3), padding=1, num_groups=8, pool_kernal=2):
+    super(Res_Unet, self).__init__()
+    encoder_list = []
+    for i in range(len(out_channels)):
+      if i==0:
+        encoder = res_block(in_channels, out_channels[i], kernel_size, padding, num_groups)
+      else:
+        encoder = encoder_block(out_channels[i-1], out_channels[i], kernel_size, padding, num_groups, pool_kernal)
+      encoder_list.append(encoder)
+    self.encoders = nn.ModuleList(encoder_list)
+    
+    decoder_list = []
+    reversed_channels = list(reversed(out_channels))
+    for i in range(len(reversed_channels)-1):
+      decoder = decoder_block(reversed_channels[i], reversed_channels[i+1], kernel_size, padding, num_groups)
+      decoder_list.append(decoder)
+    self.decoders = nn.ModuleList(decoder_list)
        
-        decoder_list = []
-        reversed_channels = list(reversed(out_channels))
-        for i in range(len(reversed_channels)-1):
-            curr_in_channels = reversed_channels[i]+reversed_channels[i+1] #######is this relevent if we dont concatinate
-            decoder = decoder_block(curr_in_channels, reversed_channels[i+1], kernel_size, padding, num_groups)
-            decoder_list.append(decoder)
-        self.decoders = nn.ModuleList(decoder_list)
-       
-        self.final = nn.Sequential(
-            nn.Conv3d(out_channels[0], in_channels, kernel_size=(1,1,1)),
-            nn.Softmax(dim=1)
-            )
-
+    self.final = nn.Sequential(
+      nn.Conv3d(out_channels[0], in_channels, kernel_size=(1,1,1)),
+      nn.Softmax(dim=1)
+    )
+    
     def forward(self, x):
-        encoder_fmap = []
-        for encoder in self.encoders:
-            x = encoder(x)
-            encoder_fmap.append(x)
-        r_encoder_fmap = list(reversed(encoder_fmap))
-        r_encoder_fmap = r_encoder_fmap[1:]
-        for i, decoder in enumerate(self.decoders):
-            x = decoder(x, r_encoder_fmap[i])
+      encoder_fmap = []
+      for encoder in self.encoders:
+        x = encoder(x)
+        encoder_fmap.append(x)
+      r_encoder_fmap = list(reversed(encoder_fmap))
+      r_encoder_fmap = r_encoder_fmap[1:]
+      for i, decoder in enumerate(self.decoders):
+        x = decoder(x, r_encoder_fmap[i])
        
-        x = self.final(x)
-        return x
+      x = self.final(x)
+      return x
--- Vnet_3D.py
+++ Vnet_3D.py
@@ -1,12 +1,10 @@
 import torch
 import torch.nn as nn
-
-#check chennels
 def first_conv(in_channels, out_channels, kernel_size=(5,5,5), padding=1):
   return nn.Sequential(
-      nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
-      nn.InstanceNorm3d(out_channels)
-      )
+    nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+    nn.InstanceNorm3d(out_channels)
+  )
   
 class first_conv_block(nn.Module):
   def __init__(in_channels, out_channels):
@@ -19,27 +17,28 @@ class first_conv_block(nn.Module):
 
 def conv_block(in_channels, out_channels, kernel_size=(5,5,5), padding=2):
   return nn.Sequential(
-     nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
-     nn.InstanceNorm3d(out_channels),
-     nn.PReLU()
-     )
+    nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+    nn.InstanceNorm3d(out_channels),
+    nn.PReLU()
+  )
   
 #this is not for thefirst conv
 def create_convs(num_convs, in_channels, out_channels):
   modules = nn.ModuleList([])
-  for i in range(num_convs):#change in_channels
-    module.append(conv_block(in_channels, out_channels))
+  modules.append(conv_block(in_channels, out_channels))
+  for i in range(1, num_convs):
+    module.append(conv_block(out_channels, out_channels))
   return modules
 
 class encoder_block(nn.Module):
   def __init__(self,num_convs, in_channels, out_channels):
     self.conv = nn.Sequential(create_convs(num_convs, in_channels, out_channels))
     self.down = nn.Sequential(
-        nn.Conv3d(in_channels, out_channels, kernel_size=(2,2,2), stride=2),
-        nn.InstanceNorm3d(out_channels),
-        nn.PReLU(),
-        nn.Dropout3d()
-        )
+      nn.Conv3d(in_channels, out_channels, kernel_size=(2,2,2), stride=2),
+      nn.InstanceNorm3d(out_channels),
+      nn.PReLU(),
+      nn.Dropout3d()
+    )
     self.prelu = nn.PReLU()
   def forward(self,x):
     x = down(x)
@@ -52,14 +51,14 @@ class encoder_block(nn.Module):
     return x
 
 class decoder_block(nn.Module):
-  def __init__(self,num_convs, in_channels, out_channels):
+  def __init__(self, num_convs, in_channels, out_channels):
     self.up = nn.Sequential(
-        nn.ConvTranspose3d(in_channels, out_channels, kernel_size=(2,2,2), stride=2),
-        nn.InstanceNorm3d(out_channels),
-        nn.PReLU(),
-        nn.Dropout3d()
-        )
-    self.conv = nn.Sequential(create_convs(num_convs, in_channels, out_channels))
+      nn.ConvTranspose3d(in_channels, out_channels, kernel_size=(2,2,2), stride=2),
+      nn.InstanceNorm3d(out_channels),
+      nn.PReLU(),
+      nn.Dropout3d()
+    )
+    self.conv = nn.Sequential(create_convs(num_convs, in_channels+out_channels, out_channels))
     self.prelu = nn.PReLU()
   def forward(self,x, encoder_fmap):
     x = up(x)
@@ -76,11 +75,11 @@ class last_conv_block(nn.Module):
   def __init__(self,in_channels, out_channels):
     self.conv = conv_block(in_channels, out_channels)
     self.up = nn.Sequential(
-        nn.ConvTranspose3d(in_channels, out_channels, kernel_size=(2,2,2) stride=2),
-        nn.InstanceNorm3d(out_channels),
-        nn.PReLU(),
-        nn.Dropout3d()
-        )
+      nn.ConvTranspose3d(in_channels, out_channels, kernel_size=(2,2,2) stride=2),
+      nn.InstanceNorm3d(out_channels),
+      nn.PReLU(),
+      nn.Dropout3d()
+    )
     self.final = nn.Sequential(nn.Conv3d(in_channels, out_channels, kernel_size=(1,1,1)),
                                nn.Softmax(dim=1))
   def forward(self, x, encoder_fmap):
@@ -89,51 +88,43 @@ class last_conv_block(nn.Module):
     x = conv(x)
     x = final(x)
     return x
-
-
+  
 #################################################
 ########  vnet  #################################
 #################################################
 
 class VNet(nn.Module):
-    def __init__(self, in_channels=4, out_channels=[32,64,128,256,521], num_convs=[1,2,3,3,4]):
-        super(UNet_3D, self).__init__()
-        #encoder
-        encoder_list = []
-        for i in range(len(out_channels)):
-            if i==0:
-                encoder = first_conv_block(in_channels, out_channels[i])
-            else:
-                encoder = encoder_block(num_convs=num_convs[i], out_channels[i-1], out_channels[i])
-            encoder_list.append(encoder)
-        self.encoders = nn.ModuleList(encoder_list)
-        #decoder
-        decoder_list = []
-        reversed_channels = list(reversed(out_channels))
-        reversed_channels = reversed_channels[1:]
-        reversed_num_convs = list(reversed(out_channels))
-        reversed_num_convs = reversed_num_convs[1:]
-        for i in range(len(reversed_channels)-1):
-          if i==#last one use the last decoder block
-            curr_in_channels = reversed_channels[i]+reversed_channels[i+1]
-            decoder = decoder_block(num_convs=reversed_num_convs[i], curr_in_channels, reversed_channels[i+1])
-            decoder_list.append(decoder)
-        self.decoders = nn.ModuleList(decoder_list)
-       
-        self.final = nn.Sequential(
-            nn.Conv3d(out_channels[0], in_channels, kernel_size=(1,1,1)),
-            nn.Softmax(dim=1)
-            )
+  def __init__(self, in_channels=4, out_channels=[32,64,128,256,521], num_convs=[1,2,3,3,4]):
+    super(UNet_3D, self).__init__()
+    #encoder
+    encoder_list = []
+    encoder = first_conv_block(in_channels, out_channels[i])
+    encoder_list.append(encoder)
+    for i in range(1, len(out_channels)):
+      encoder = encoder_block(num_convs=num_convs[i], out_channels[i-1], out_channels[i])
+      encoder_list.append(encoder)
+    self.encoders = nn.ModuleList(encoder_list)
+    #decoder
+    decoder_list = []
+    reversed_channels = reversed_channels[1:]
+    reversed_channels = list(reversed(out_channels))#[521,256,128,64]
+    reversed_num_convs = reversed_num_convs[1:]
+    reversed_num_convs = list(reversed(out_channels))#[3,3,2,1]
+    for i in range(len(reversed_channels)-1):
+      decoder = decoder_block(num_convs=reversed_num_convs[i], reversed_channels[i], reversed_channels[i+1])
+      decoder_list.append(decoder)
+    last_decoder = last_conv_block(num_convs[0], out_channels[0], in_channels)
+    decoder_list.append(last_decoder)
+    self.decoders = nn.ModuleList(decoder_list)
            
-    def forward(self, x):
-        encoder_fmap = []
-        for encoder in self.encoders:
-            x = encoder(x)
-            encoder_fmap.append(x)
-        r_encoder_fmap = list(reversed(encoder_fmap))
-        #the last output is of the middle layer that doesnt get a res
-        r_encoder_fmap = r_encoder_fmap[1:]
-        for i, decoder in enumerate(self.decoders):
-            x = decoder(x, r_encoder_fmap[i])
-
-        return x
+  def forward(self, x):
+    encoder_fmap = []
+    for encoder in self.encoders:
+      x = encoder(x)
+      encoder_fmap.append(x)
+    r_encoder_fmap = list(reversed(encoder_fmap))
+    #the last output is of the middle layer that doesnt get a res
+    r_encoder_fmap = r_encoder_fmap[1:]
+    for i, decoder in enumerate(self.decoders):
+      x = decoder(x, r_encoder_fmap[i])
+    return x
